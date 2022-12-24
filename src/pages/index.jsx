41007564b7e1342c0187b3a73f59d2c90c4a83da
Komakine Anna <bbxxuw@gmail.com>
2022-12-24 11:44:46 +0900

    ✨ カウンターを設置してuseStateでカウントアップする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,16 +3,19 @@ import classes from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [count, setCount] = useState(1);
+
+  const handleClick = (e) => {
+    setCount(count => count + 1);
+  };
 
   useEffect(() => {
-    console.log('マウント時');
     document.body.style.backgroundColor = "lightBlue";
     // アンマウント時の処理
     return () => {
-      console.log('アンマウント時');
       document.body.style.backgroundColor = "";
     };
   }, []);
@@ -23,7 +26,8 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      {/* <a href="/about" onClick={handleClick}>ボタン</a> */}
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
