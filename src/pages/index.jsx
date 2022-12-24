1f9a2aef477e4b00b63f32ffdedae11657425f9f
Komakine Anna <bbxxuw@gmail.com>
2022-12-24 11:21:07 +0900

    ✨ 説明用のuseEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,27 @@ import classes from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    console.log('マウント時');
+    document.body.style.backgroundColor = "lightBlue";
+    // アンマウント時の処理
+    return () => {
+      console.log('アンマウント時');
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   return (
     <div className={classes.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      {/* <a href="/about" onClick={handleClick}>ボタン</a> */}
       <Main page="index" />
       <Footer />
     </div>
